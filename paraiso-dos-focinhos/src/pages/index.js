d7a4b94060d3e5c86489a30544869d315934a778
Mateus Santos <mateus@pop-os.localdomain>
2020-11-03 16:43:20 -0300

    starting footer

--- paraiso-dos-focinhos/src/pages/index.js
+++ paraiso-dos-focinhos/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
-import HelpSection from '../components/HelpSection' 
+import HelpSection from '../components/HelpSection'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
   <HelpSection />
+  <Footer />
   )
 }
